d496e01ed1131d4b37c4978a20239fabb2d7a798
hong <hhj749110@gmail.com>
2024-01-19 13:47:25 +0900

    240119

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-const server = io("http://localhost:5000/");
-//const server = io("https://chat-server-xoxj.onrender.com");
+// const server = io("http://localhost:5000/");
+const server = io("https://chat-server-xoxj.onrender.com");
 export default server;
